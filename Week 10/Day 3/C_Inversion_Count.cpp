de000e80f237f7a2b61696b4f89fe390b60b78bd
Sourav_islam <souravislam267@gmail.com>
2024-05-27 23:39:50 +0600

    only 1 problem

---
+++ Week 10/Day 3/C_Inversion_Count.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+            cin >> v[i];
+
+        pbds<int> p;
+        long long count = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            count += p.order_of_key(v[i]);
+            p.insert(v[i]);
+        }
+        cout << count << '\n';
+    }
+    return 0;
+}
